f83f67d5b839c5b99a4c4ca92497fa93442eac24
Arllain <arllain@gmail.com>
2021-10-13 20:41:42 -0300

    added a UICollectionView

--- JewelleryApp/JewelleryApp/Home/HomeViewController.swift
+++ JewelleryApp/JewelleryApp/Home/HomeViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 final class HomeViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundGradient()
         configureNavigationBar()
         configureSearchTextField()
+        configureCollectionView()
     }
 }
 
@@ -48,5 +50,32 @@ private extension HomeViewController {
 
         view.layer.insertSublayer(gradient, at: 0)
     }
+    
+    func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "CardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier:  "cardCollectionViewCellID")
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 234, height: 300)
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        collectionView.showsHorizontalScrollIndicator = false
+    }
 
+}
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCollectionViewCellID", for: indexPath) as? CardCollectionViewCell else {
+            fatalError()
+        }
+         
+        cell.configure(model: CardCollectionViewCellModel(imageName: "diamond_ring", title: "Diamond Ring", subtitle: "Pure ring with Diamonds (0.1400 Ct)", price: "600"))
+        return cell
+    }
 }
